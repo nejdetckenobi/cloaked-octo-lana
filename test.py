df1a1c3624cf14ec85317e98ff773582aa88b282
Nejdet Yucesoy <nejdetyucesoy@gmail.com>
2015-06-04 14:09:24 +0300

    test file modified.

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import unittest
 
+
+class FastTest(unittest.TestCase):
+    def setUp(self):
+        super(FastTest, self).setUp()
+
+    def test_test(self):
+        self.assertEqual(True, True)
+
+    def tearDown(self):
+        super(FastTest, self).tearDown()
+
+
 unittest.main()
